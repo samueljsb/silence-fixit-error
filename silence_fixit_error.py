ca54d342f3d3c081263b6c3c588ca35eaf5f0a08
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2023-10-10 17:41:27 +0100

    First implementation
    Co-authored-by: Charlie Denton <charlie@meshy.co.uk>

---
+++ silence_fixit_error.py
@@ -0,0 +1,108 @@
+from __future__ import annotations
+
+import argparse
+import subprocess
+import sys
+from collections import defaultdict
+from collections.abc import Iterator
+from collections.abc import Sequence
+from typing import NamedTuple
+
+
+class Violation(NamedTuple):
+    rule_name: str
+    lineno: int
+
+
+def _find_violations(
+        rule_name: str, filenames: Sequence[str],
+) -> dict[str, list[Violation]]:
+    proc = subprocess.run(
+        (
+            sys.executable, '-mfixit',
+            '--rules', rule_name,
+            'lint', *filenames,
+        ),
+        capture_output=True,
+        text=True,
+    )
+
+    # extract filenames and line numbers
+    results: dict[str, list[Violation]] = defaultdict(list)
+    for line in proc.stdout.splitlines():
+        location, violated_rule_name, __ = line.split(maxsplit=2)
+        filename, position = location.split('@', maxsplit=1)
+        lineno, __ = position.split(':', maxsplit=1)
+
+        rule_name_ = violated_rule_name.removesuffix(':')
+
+        results[filename].append(Violation(rule_name_, int(lineno)))
+
+    return results
+
+
+def _add_comments(
+        lines: Sequence[str],
+        linenos_to_silence: set[int], rule_name: str,
+) -> Iterator[str]:
+    for current_lineno, line in enumerate(lines, start=1):
+        if current_lineno in linenos_to_silence:
+            leading_ws = line.removesuffix(line.lstrip())
+            yield f'{leading_ws}# lint-fixme: {rule_name}\n'
+        yield line
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('rule_name')
+    parser.add_argument('filenames', nargs='*')
+    args = parser.parse_args(argv)
+
+    print('-> running fixit', file=sys.stderr)
+    violations = _find_violations(args.rule_name, args.filenames)
+
+    if not violations:
+        print('no violations found', file=sys.stderr)
+        return 0
+
+    print(f'found violations in {len(violations)} files', file=sys.stderr)
+
+    violation_names = {
+        violation.rule_name
+        for file_violations in violations.values()
+        for violation in file_violations
+    }
+    if len(violation_names) != 1:
+        print(
+            'ERROR: errors found for multiple rules:',
+            sorted(violation_names),
+            file=sys.stderr,
+        )
+        return 1
+
+    [violation_name] = violation_names
+
+    print('-> adding fixme comments', file=sys.stderr)
+    ret = 0
+    for filename, file_violations in violations.items():
+        print(filename, file=sys.stderr)
+        with open(filename) as f:
+            src = f.read()
+
+        lines = src.splitlines(keepends=True)
+        src_with_comments = ''.join(
+            _add_comments(
+                lines, {v.lineno for v in file_violations}, violation_name,
+            ),
+        )
+
+        with open(filename, 'w') as f:
+            f.write(src_with_comments)
+
+        ret |= src_with_comments != src
+
+    return ret
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
---
+++ tests/silence_fixit_error_test.py
@@ -0,0 +1,98 @@
+from __future__ import annotations
+
+from silence_fixit_error import _find_violations
+from silence_fixit_error import main
+from silence_fixit_error import Violation
+
+
+def test_find_violations(tmp_path, capsys):
+    python_module = tmp_path / 't.py'
+    python_module.write_text("""\
+x = None
+isinstance(x, str) or isinstance(x, int)
+""")
+
+    violations = _find_violations(
+        'fixit.rules:CollapseIsinstanceChecks', [str(python_module)],
+    )
+
+    assert violations == {
+        str(python_module): [Violation('CollapseIsinstanceChecks', 2)],
+    }
+
+
+def test_main(tmp_path, capsys):
+    python_module = tmp_path / 't.py'
+    python_module.write_text("""\
+x = None
+isinstance(x, str) or isinstance(x, int)
+
+def f(x):
+    return isinstance(x, str) or isinstance(x, int)
+""")
+
+    ret = main(('fixit.rules:CollapseIsinstanceChecks', str(python_module)))
+
+    assert ret == 1
+    assert python_module.read_text() == """\
+x = None
+# lint-fixme: CollapseIsinstanceChecks
+isinstance(x, str) or isinstance(x, int)
+
+def f(x):
+    # lint-fixme: CollapseIsinstanceChecks
+    return isinstance(x, str) or isinstance(x, int)
+"""
+
+    captured = capsys.readouterr()
+    assert captured.err == f"""\
+-> running fixit
+found violations in 1 files
+-> adding fixme comments
+{python_module}
+"""
+
+
+def test_main_no_violations(tmp_path, capsys):
+    src = """\
+def foo():
+    print('hello there')
+"""
+
+    python_module = tmp_path / 't.py'
+    python_module.write_text(src)
+
+    ret = main(('fixit.rules:CollapseIsinstanceChecks', str(python_module)))
+
+    assert ret == 0
+    assert python_module.read_text() == src
+
+    captured = capsys.readouterr()
+    assert captured.err == """\
+-> running fixit
+no violations found
+"""
+
+
+def test_main_multiple_different_violations(tmp_path, capsys):
+    src = """\
+x = None
+isinstance(x, str) or isinstance(x, int)
+
+if True:
+    pass
+"""
+
+    python_module = tmp_path / 't.py'
+    python_module.write_text(src)
+
+    ret = main(('fixit.rules', str(python_module)))
+
+    assert ret == 1
+
+    captured = capsys.readouterr()
+    assert captured.err == """\
+-> running fixit
+found violations in 1 files
+ERROR: errors found for multiple rules: ['CollapseIsinstanceChecks', 'NoStaticIfCondition']
+"""  # noqa: E501
